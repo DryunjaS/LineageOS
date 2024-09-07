4dbdc0f1e48e4113766a94e1a8cfe5e20aff92fc
Sosnin Andrey <KamushekKamushek@gmail.com>
2024-09-07 14:49:27 +0300

    fix: search-loading

--- frontend/src/components/NavDevice.tsx
+++ frontend/src/components/NavDevice.tsx
@@ -56,16 +56,22 @@ const NavDevice = () => {
             <path d="M0 0h24v24H0V0z" fill="none" />
             <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
           </svg>
-          <input
-            type="text"
-            placeholder="Поиск"
-            className="input-device"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setTimeout(() => setIsFocused(false), 200)} // Задержка перед потерей фокуса
-          />
-          {search.length !== 0 && isFocused && <SearchList search={search} />}
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Поиск"
+              className="input-device"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setTimeout(() => setIsFocused(false), 200)}
+            />
+            <div className="absolute -right-10 top-20">
+              {search.length !== 0 && isFocused && (
+                <SearchList search={search} />
+              )}
+            </div>
+          </div>
         </div>
         <div className="mr-5 lg:hidden">
           <button
@@ -130,11 +136,23 @@ const NavDevice = () => {
             <path d="M0 0h24v24H0V0z" fill="none" />
             <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
           </svg>
-          <input
-            type="text"
-            placeholder="Поиск"
-            className="input-device w-[170px]"
-          />
+
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Поиск"
+              className="input-device w-[170px]"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setTimeout(() => setIsFocused(false), 200)}
+            />
+            <div className="absolute -right-8 top-14">
+              {search.length !== 0 && isFocused && (
+                <SearchList search={search} />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </nav>
--- frontend/src/components/SearchList.tsx
+++ frontend/src/components/SearchList.tsx
@@ -4,10 +4,14 @@ import { DevicesGroupItemType, DevicesGroupType } from '../page/DevicesADMIN'
 import { useNavigate } from 'react-router-dom'
 
 const SearchList = ({ search }: { search: string }) => {
-  const [filteredList, setFilteredList] = useState<DevicesGroupItemType[]>([]) // Массив строк
+  const [filteredList, setFilteredList] = useState<DevicesGroupItemType[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+
   const navigate = useNavigate()
 
   const searchToPage = async () => {
+    setIsLoading(true)
+
     try {
       const response: DevicesGroupType[] = await getDevicesGroupedByVendor()
 
@@ -23,37 +27,68 @@ const SearchList = ({ search }: { search: string }) => {
     } catch (error) {
       console.error('Error fetching devices:', error)
       return []
+    } finally {
+      setIsLoading(false)
     }
   }
 
   useEffect(() => {
     const fetchAndFilterData = async () => {
       const filteredData = await searchToPage()
-      setFilteredList(filteredData) // Обновляем массив названий моделей
+      setFilteredList(filteredData)
     }
 
     fetchAndFilterData()
   }, [search])
 
   const onSelect = (deviceCode: string, deviceID: number | null) => {
-    console.log(deviceID)
     sessionStorage.setItem('tmp', `${deviceID}`)
     navigate(`/devices/${deviceCode}`)
   }
 
   return (
-    <div className="absolute right-5 top-20 z-20 w-64 rounded-lg bg-white p-4 shadow-lg">
-      <ul className="divide-y divide-gray-200">
-        {filteredList.map((device, index) => (
-          <li
-            key={index}
-            className="cursor-pointer p-2 text-primary transition-colors duration-200 hover:bg-gray-100"
-            onMouseDown={() => onSelect(device.name.Code, device.id)} // Используем onMouseDown
+    <div className="z-20 w-64 rounded-lg bg-white p-4 shadow-lg">
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <svg
+            className="mr-2 h-5 w-5 animate-spin text-primary"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
           >
-            {device.name.Model}
-          </li>
-        ))}
-      </ul>
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+            ></path>
+          </svg>
+          Загрузка...
+        </div>
+      ) : filteredList.length === 0 ? (
+        <div className="text-center text-gray-500">
+          Нет результатов для "{search}"
+        </div>
+      ) : (
+        <ul className="max-h-[200px] divide-y divide-gray-200 overflow-y-auto md:max-h-[400px]">
+          {filteredList.map((device, index) => (
+            <li
+              key={index}
+              className="cursor-pointer p-2 text-primary transition-colors duration-200 hover:bg-gray-100"
+              onMouseDown={() => onSelect(device.name.Code, device.id)}
+            >
+              {device.name.Model}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
